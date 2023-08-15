bc27591706cccccb2b5aaeb48287f5fb04b58666
Fv-store <141719506+Fv-store@users.noreply.github.com>
2023-08-15 19:37:36 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -7,8 +7,8 @@ BURIQ () {
     exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
     d1=(`date -d "$exp" +%s`)
     d2=(`date -d "$biji" +%s`)
-    expd=$(( (d1 - d2) / 86400 ))
-    if [[ "$expd" -le "0" ]]; then
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
     echo $user > /etc/.$user.ini
     else
     rm -f /etc/.$user.ini > /dev/null 2>&1
@@ -17,14 +17,16 @@ BURIQ () {
     rm -f /root/tmp
 }
 
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Names=$(curl -sS https://raw.githubusercontent.com/Fv-store/my-ip/main/izin | grep $MYIP | awk '{print $2}')
-echo $Names > /usr/local/etc/.$Names.ini
-CekOne=$(cat /usr/local/etc/.$Names.ini)
+Name=$(curl -sS https://raw.githubusercontent.com/Fv-store/my-ip/main/izin | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
 
 Bloman () {
-if [ -f "/etc/.$Names.ini" ]; then
-CekTwo=$(cat /etc/.$Names.ini)
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
     if [ "$CekOne" = "$CekTwo" ]; then
         res="Expired"
     fi
@@ -39,10 +41,12 @@ PERMISSION () {
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
-    res="Blm Izin Lu Ama gw 🗿"
+    res="Permission Denied!"
     fi
     BURIQ
 }
+COLOR1='\033[0;33m'
+COLOR2='\033[0;39m'
 red='\e[1;31m'
 green='\e[1;32m'
 NC='\e[0m'
@@ -51,13 +55,11 @@ red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 PERMISSION
 
 if [ "$res" = "Expired" ]; then
-Expt="\e[36mExpired\033[0m"
+Exp="\e[36mExpired\033[0m"
 else
-Expt=$(curl -sS https://raw.githubusercontent.com/Fv-store/my-ip/main/izin | grep $MYIP | awk '{print $3}')
+Exp=$(curl -sS https://raw.githubusercontent.com/Fv-store/my-ip/main/izin | grep $MYIP | awk '{print $3}')
 fi
-####
-MYIP=$(curl -sS ipv4.icanhazip.com)
-#########################
+
 
 # Color Validation
 DF='\e[39m'
@@ -210,8 +212,7 @@ echo -e "  ${LIGHT}• ${CYAN}Server IP           ${NC}=${BIGreen} $IPVPS"
 echo -e "  ${LIGHT}• ${CYAN}ISP-VPS             ${NC}=${BIGreen} $ISP"
 echo -e "  ${LIGHT}• ${CYAN}City                ${NC}=${BIGreen} $CITY"
 echo -e "  ${LIGHT}• ${CYAN}Clients Name        ${NC}=${BIYellow} $Name"
-echo -e "  ${LIGHT}• ${CYAN}Script Expired      ${NC}=${BIYellow} $Expt"
-echo -e "  ${LIGHT}• ${CYAN}Durasi Expired      ${NC}=${BIYellow} $Expd Hari"
+echo -e "  ${LIGHT}• ${CYAN}Script Expired      ${NC}=${BIYellow} $Exp"
 echo -e "${CYAN}┌────────────────────────────────────────────────────────────┐${NC}"
 echo -e "                     << STATUS SERVICE >>                    \E[0m" | lolcat    
 echo -e "${CYAN}┌────────────────────────────────────────────────────────────┐${NC}"
