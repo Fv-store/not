34956dc4d0a3196ecc7d5cf6081e4d8ad2f080d6
Fv-store <141719506+Fv-store@users.noreply.github.com>
2023-08-19 17:23:28 +0700

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -70,5 +70,4 @@ echo -e "Expired On     : $exp" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
-
-menu
+menu-vless
